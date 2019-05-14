359b0a904c49bfba13da985a5c4b4c12d0efebd6
Raphael Castilho Gil <raphacgil@gmail.com>
2019-05-13 23:26:34 -0300

    prepare json

---
+++ src/util/json_prepare.py
@@ -0,0 +1,19 @@
+"""
+subroutine to prepare json to be sent or read from mongodb
+"""
+
+class JsonPrepare:
+
+    def local_mongo(self, local_list):
+        """
+        :param local_list: All data stored in list to be prepared in json
+        :return: return a list of json prepared
+        """
+        pass
+
+    def message_doctor(self, remote_info):
+        """
+        :param remote_info: Return remote info
+        :return: return a list of json prepared
+        """
+        pass
